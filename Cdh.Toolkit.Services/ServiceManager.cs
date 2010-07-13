7e9851377d24112dbd4f2c9b3c31cf05f3d5711a
Chris Howie <me@chrishowie.com>
2010-07-13 15:01:13 -0400

    Added Cdh.Toolkit.Services library

---
+++ Cdh.Toolkit.Services/ServiceManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Cdh.Toolkit.Collections;
+
+namespace Cdh.Toolkit.Services
+{
+	public class ServiceManager
+	{
+		private SynchronizedCollection<IService> services =
+			new SynchronizedCollection<IService>(new HashSet<IService>(), EnumerateBehavior.Lock);
+
+		public ServiceManager() { }
+
+		public void RegisterService(IService service)
+		{
+			if (service == null)
+				throw new ArgumentNullException("service");
+
+			services.Add(service);
+		}
+
+		public void UnregisterService(IService service)
+		{
+			if (service == null)
+				throw new ArgumentNullException("service");
+
+			services.Remove(service);
+		}
+
+		public T GetService<T>() where T : IService
+		{
+			return services.OfType<T>().SingleOrDefault();
+		}
+	}
+}
---
+++ Cdh.Toolkit.Services/ThreadedService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Cdh.Toolkit.Services
+{
+	public abstract class ThreadedService : IService
+	{
+		private object sync = new object();
+
+		protected bool ThreadRunning { get; private set; }
+
+		private Thread thread = null;
+
+		#region IService Members
+
+		public virtual void Start()
+		{
+			lock (sync)
+			{
+				if (IsRunning || ThreadRunning)
+					return;
+
+				thread = new Thread(ServiceLoop);
+				IsRunning = true;
+				ThreadRunning = true;
+
+				thread.Start();
+			}
+		}
+
+		public virtual void Stop()
+		{
+			lock (sync)
+			{
+				if (ThreadRunning == false)
+					return;
+
+				ThreadRunning = false;
+				thread.Interrupt();
+				thread.Join();
+
+				thread = null;
+				IsRunning = false;
+			}
+		}
+
+		protected abstract void ServiceLoop();
+
+		public bool IsRunning { get; private set; }
+
+		#endregion
+	}
+}
