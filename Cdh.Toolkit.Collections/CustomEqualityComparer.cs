66aeb10c78e33386ead09ff381a6ec858104a137
Chris Howie <me@chrishowie.com>
2011-05-09 14:15:30 -0400

    Add CustomEqualityComparer

---
+++ Cdh.Toolkit.Collections/CustomEqualityComparer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cdh.Toolkit.Collections
+{
+    public class CustomEqualityComparer<TObject, TKey> : IEqualityComparer<TObject>
+    {
+        private Func<TObject, TKey> keySelector;
+
+        public CustomEqualityComparer(Func<TObject, TKey> keySelector)
+        {
+            if (keySelector == null)
+                throw new ArgumentNullException("keySelector");
+
+            this.keySelector = keySelector;
+        }
+
+        #region IEqualityComparer<TObject> Members
+
+        public bool Equals(TObject x, TObject y)
+        {
+            return EqualityComparer<TKey>.Default.Equals(keySelector(x), keySelector(y));
+        }
+
+        public int GetHashCode(TObject obj)
+        {
+            return EqualityComparer<TKey>.Default.GetHashCode(keySelector(obj));
+        }
+
+        #endregion
+    }
+}
