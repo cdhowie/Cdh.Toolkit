d1d547120d2b481d8853ca516327c0c47dab61ac
Chris Howie <me@chrishowie.com>
2011-06-30 18:47:45 -0400

    Add new Check methods and fix cosmetic bug in Check.ArgumentIsNotNull

--- Cdh.Toolkit.Extensions/Check.cs
+++ Cdh.Toolkit.Extensions/Check.cs
@@ -10,7 +10,28 @@ namespace Cdh.Toolkit.Extensions
         public static void ArgumentIsNotNull(object value, string paramName)
         {
             if (value == null)
-                throw new ArgumentNullException("paramName");
+                throw new ArgumentNullException(paramName);
+        }
+
+        public static T ArgumentIs<T>(object value, string paramName)
+        {
+            Check.ArgumentIsNotNull(value, paramName);
+
+            return OptionalArgumentIs<T>(value, paramName);
+        }
+
+        public static T OptionalArgumentIs<T>(object value, string paramName)
+        {
+            if (value == null)
+                return default(T);
+
+            if (value is T)
+                return (T)value;
+
+            throw new ArgumentException(
+                string.Format("Expected object of type {0}, but received object of type {1}.",
+                    typeof(T).FullName, value.GetType().FullName),
+                paramName);
         }
     }
 }
