c366e9879618a237f7f83e1118e278da848f6a36
Chris Howie <me@chrishowie.com>
2015-02-09 08:58:25 -1000

    Add NullableUtility class

---
+++ Cdh.Toolkit.Extensions/Nullable/NullableUtility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Cdh.Toolkit.Extensions.Nullable
+{
+    public static class NullableUtility
+    {
+        public static TResult? Convert<TSource, TResult>(this TSource? source, Func<TSource, TResult> conversion)
+            where TSource : struct
+            where TResult : struct
+        {
+            Check.ArgumentIsNotNull(conversion, "conversion");
+
+            return source.HasValue ? conversion(source.Value) : (TResult?)null;
+        }
+    }
+}
+
