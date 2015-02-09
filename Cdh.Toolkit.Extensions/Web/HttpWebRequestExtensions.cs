86f30baf2e14019997ad8e6b5515ab7cdb9f67a4
Chris Howie <me@chrishowie.com>
2015-02-08 21:11:34 -1000

    Add extension for POSTing application/x-www-form-urlencoded data

---
+++ Cdh.Toolkit.Extensions/Web/HttpWebRequestExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Collections.Specialized;
+using System.Text;
+using System.IO;
+
+namespace Cdh.Toolkit.Extensions.Web
+{
+    public static class HttpWebRequestExtensions
+    {
+        public static void PostWwwFormUrlencoded(this HttpWebRequest request, NameValueCollection form)
+        {
+            Check.ArgumentIsNotNull(request, "request");
+            Check.ArgumentIsNotNull(form, "form");
+
+            var encoding = new UTF8Encoding(false);
+
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded; charset=" + encoding.WebName;
+
+            using (var writer = new StreamWriter(request.GetRequestStream(), encoding)) {
+                writer.Write(form.ToQueryString());
+            }
+        }
+    }
+}
+
