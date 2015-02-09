cec901505c8026325d33ffc71f883b957a549d3b
Chris Howie <me@chrishowie.com>
2015-02-08 16:44:47 -1000

    Add NameValueCollectionExtensions

---
+++ Cdh.Toolkit.Extensions/Web/NameValueCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Web;
+using System.Linq;
+using Cdh.Toolkit.Extensions.Enumerable;
+
+namespace Cdh.Toolkit.Extensions.Web
+{
+    public static class NameValueCollectionExtensions
+    {
+        public static string ToQueryString(this NameValueCollection self)
+        {
+            Check.ArgumentIsNotNull(self, "self");
+
+            return (
+                from key in self.AllKeys
+                from value in self.GetValues(key)
+                select HttpUtility.UrlEncode(key) + "=" + HttpUtility.UrlEncode(value)
+            ).Join("&");
+        }
+    }
+}
+
